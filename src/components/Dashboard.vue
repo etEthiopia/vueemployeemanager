064b32f4f135ff484daf9df00f61b0b000dd1fc3
Dagmawi Negussu <5902034-daginegussu@users.noreply.gitlab.com>
2020-06-08 17:09:56 +0300

    Dashboard

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="dashboard">
+        <h2>Dashboard</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'dashboard',
+    data() {
+        return {
+            
+        }
+    },
+}
+</script>
