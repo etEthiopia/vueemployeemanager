18201f09ac02bdd517fcb7eec47d6e170c0bde6d
Dagmawi Negussu <5902034-daginegussu@users.noreply.gitlab.com>
2020-06-08 17:12:28 +0300

    View Edit Employee

---
+++ src/components/EditEmployee.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="edit_employee">
+        <h2>Edit employee</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'edit_employee',
+    data() {
+        return {
+            
+        }
+    },
+}
+</script>
---
+++ src/components/ViewEmployee.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="view_employee">
+        <h2>View employee</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'view_employee',
+    data() {
+        return {
+            
+        }
+    },
+}
+</script>
