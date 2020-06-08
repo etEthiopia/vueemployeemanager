6361f9f12752df08166262acd3d8e6ccbec99979
Dagmawi Negussu <5902034-daginegussu@users.noreply.gitlab.com>
2020-06-08 21:30:42 +0300

    to view employee

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,6 +1,20 @@
 <template>
     <div id="dashboard">
-        <h2>Dashboard</h2>
+        <ul class="collection with-header">
+            <li class="collection-header">
+                <h4>Employees</h4>
+            </li>
+            <li class="collection-item" v-for="employee in employees" v-bind:key="employee.id">
+                
+                {{employee.employeeid}} : {{employee.name}}
+            <div class="chip">
+                    {{employee.dept}}
+                </div>
+                <router-link class="secondary-content" v-bind:to="{name: 'view_employee', params: {employee_id: employee.employeeid}}">
+                    <li class="fa fa-eye"></li>
+                </router-link>
+            </li>
+        </ul>
         <div class="fixed-action-btn">
             <router-link to ="/new" class="btn-floating btn-large red">
                 <i class="fa fa-plus"></i>
@@ -28,7 +42,7 @@ export default {
                     'dept': doc.data().dept,
                     'position': doc.data().position
                 }
-               
+               this.employees.push(data);
             })
         })
     }
