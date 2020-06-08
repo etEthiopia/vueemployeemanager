49242167e1162814561f4574049d25b21aa3d1d8
Dagmawi Negussu <5902034-daginegussu@users.noreply.gitlab.com>
2020-06-08 17:28:35 +0300

    Route works

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Dashboard from '@/components/Dashboard'
+import NewEmployee from '@/components/NewEmployee'
+import EditEmployee from '@/components/EditEmployee'
+import ViewEmployee from '@/components/ViewEmployee'
+
 
 Vue.use(Router)
 
@@ -8,8 +13,23 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'dashboard',
+      component: Dashboard
+    },
+    {
+      path: '/new',
+      name: 'new_employee',
+      component: NewEmployee
+    },
+    {
+      path: '/edit/:employee_id',
+      name: 'edit_employee',
+      component: EditEmployee
+    },
+    {
+      path: '/:employee_id',
+      name: 'view_employee',
+      component: ViewEmployee
     }
   ]
 })
