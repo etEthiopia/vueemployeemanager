048546916b163a6cfd64038cb070ca08dea77b09
Dagmawi Negussu <5902034-daginegussu@users.noreply.gitlab.com>
2020-06-08 17:10:46 +0300

    New Employee

---
+++ src/components/NewEmployee.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="new_employee">
+        <h2>New employee</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'new_employee',
+    data() {
+        return {
+            
+        }
+    },
+}
+</script>
