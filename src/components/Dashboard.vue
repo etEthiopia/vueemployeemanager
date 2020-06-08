dc8967fa1bfb45e0f94c6371c4e2641bc044286f
Dagmawi Negussu <5902034-daginegussu@users.noreply.gitlab.com>
2020-06-08 21:17:28 +0300

    employees retrieved

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,16 +1,36 @@
 <template>
     <div id="dashboard">
         <h2>Dashboard</h2>
+        <div class="fixed-action-btn">
+            <router-link to ="/new" class="btn-floating btn-large red">
+                <i class="fa fa-plus"></i>
+            </router-link>
+        </div>
     </div>
 </template>
 
 <script>
+import db from '../backend/FirebaseInit'
 export default {
     name: 'dashboard',
     data() {
         return {
-            
+            employees: []
         }
     },
+    created(){
+        db.collection('employees').get().then(querySnapshot => {
+            querySnapshot.forEach(doc => {
+                const data = {
+                    'id': doc.id,
+                    'employeeid': doc.data().employeeid,
+                    'name': doc.data().name,
+                    'dept': doc.data().dept,
+                    'position': doc.data().position
+                }
+               
+            })
+        })
+    }
 }
 </script>
