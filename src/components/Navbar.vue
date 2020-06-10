6557dbcf512acb492b8376c9b5a3634dd69e5fea
Dagmawi Negussu <5902034-daginegussu@users.noreply.gitlab.com>
2020-06-10 11:32:27 +0300

    Done, hosted https://vuejsdb-88a2f.firebaseapp.com

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -4,9 +4,9 @@
             <div class="container">
                 <router-link to="/" class="brand-logo left">Employee Manager</router-link>
                 <ul class="right">
-                    <li><router-link to="/login">Login</router-link></li>
-                    <li><router-link to="/register">Register</router-link></li>
-                    <li><router-link to="" @click.native="logout">Logout</router-link></li>
+                    <li v-if="!isLogged"><router-link to="/login">Login</router-link></li>
+                    <li v-if="!isLogged"><router-link to="/register">Register</router-link></li>
+                    <li v-if="isLogged"><router-link @click.native="logout"  to="" >Logout</router-link> </li>
                 </ul>
             </div>
         </div>
@@ -14,13 +14,19 @@
 </template>
 
 <script>
-import firebase from 'firebase'
+import firebase, { auth } from 'firebase'
 export default {
     data(){
         return {
             isLogged: false,
             currentUser: false
         };
+    },
+    created(){
+        if(firebase.auth().currentUser){
+            this.isLogged = true;
+            this.currentUser = firebase.auth().currentUser.email;
+        }
     },
     methods:{
         logout(e){
