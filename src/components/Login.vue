15a17289b5f861f5e164a6273fadc76f6da8575d
Dagmawi Negussu <5902034-daginegussu@users.noreply.gitlab.com>
2020-06-10 10:21:47 +0300

    REGISTERED AND LOGGED IN

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -4,23 +4,23 @@
             <div class="col s10 m6 offset-m3">
                 <div class="login card-panel green-text center">
                     <h3>Login</h3>
-                    <form action="">
+                    <form>
                         <div class="input-field">
                             <i class="material-icons prefix">
                                 email
                             </i>
-                            <input type="text" id="email">
+                            <input type="text" v-model="email" id="email">
                             <label for="email">Email</label>
                         </div>
                         <div class="input-field">
                             <i class="material-icons prefix">
                                 lock
                             </i>
-                            <input type="password" id="password">
+                            <input type="password" v-model="password" id="password">
                             <label for="password">Password</label>
                         </div>
                         <div class="right-align">
-                            <button class="btn green white-text">
+                            <button @click="login" class="btn green white-text">
                             Login
                             </button>
                         </div>
@@ -32,10 +32,28 @@
     </div>
 </template>
 <script>
+import firebase from 'firebase'
 export default {
     name: 'login',
-    data(){
-        return{};
+        data(){
+        return{
+            email: '',
+            password: ''
+        };
+    },
+    methods:{
+        login(e){
+            
+            firebase.auth().signInWithEmailAndPassword(this.email, this.password)
+            .then(user => {alert('Logged In')
+                this.$router.push('/');
+            })
+            .catch(err => alert('error: '+err.message))
+            console.log("LOGGED IN")
+            
+            e.preventDefault();
+            
+        }
     }
 }
 </script>
--- src/components/Register.vue
+++ src/components/Register.vue
@@ -1,26 +1,33 @@
 <template>
     <div class="container">
-        <div class="row center">
-            <div class="col s10 m6 offset-m3">
+        <div class="row">
+            <div class="col s12 m8 offset-m2">
                 <div class="login card-panel green-text center">
                     <h3>Register</h3>
-                    <form action="">
+                    <form>
                         <div class="input-field">
                             <i class="material-icons prefix">
                                 email
                             </i>
-                            <input type="text" id="email">
+                            <input type="text" v-model="email" id="email">
                             <label for="email">Email</label>
                         </div>
                         <div class="input-field">
                             <i class="material-icons prefix">
                                 lock
                             </i>
-                            <input type="password" id="password">
+                            <input type="password" v-model="password" id="password">
                             <label for="password">Password</label>
                         </div>
+                        <div class="input-field">
+                            <i class="material-icons prefix">
+                                lock
+                            </i>
+                            <input type="password" v-model="cpassword" id="cpassword">
+                            <label for="cpassword">Confirm Password</label>
+                        </div>
                         <div class="right-align">
-                            <button class="btn green white-text">
+                            <button @click="register" class="btn green white-text">
                             Register
                             </button>
                         </div>
@@ -32,10 +39,33 @@
     </div>
 </template>
 <script>
+import firebase from 'firebase'
 export default {
     name: 'register',
     data(){
-        return{};
+        return{
+            email: '',
+            password: '',
+            cpassword: ''
+        };
+    },
+    methods:{
+        register(e){
+            
+            if(this.password == this.cpassword){
+            firebase.auth().createUserWithEmailAndPassword(this.email, this.password)
+            .then(user => {alert('Account Created')
+                this.$router.push('/');
+            })
+            .catch(err => alert('error: '+err.message))
+            console.log("REGISTER")
+            }
+            else{
+                alert("Passwords don't match")
+            }
+            e.preventDefault();
+            
+        }
     }
 }
 </script>
