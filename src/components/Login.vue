943b5f5e78e6883aaf24e524d3c5f600e0df5637
Dagmawi Negussu <5902034-daginegussu@users.noreply.gitlab.com>
2020-06-10 09:31:40 +0300

    Login form

---
+++ src/components/Login.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col s12 m8 offset-m2">
+                <div class="login card-panel green white-text center">
+                    <h3>Login</h3>
+                    <form action="">
+                        <div class="input-field">
+                            <i class="material-icons prefix">
+                                email
+                            </i>
+                            <input type="text" id="email">
+                            <label for="email">Email</label>
+                        </div>
+                        <div class="input-field">
+                            <i class="material-icons prefix">
+                                lock
+                            </i>
+                            <input type="password" id="password">
+                            <label for="password">Password</label>
+                        </div>
+                        <button class="btn btn-large grey lighten-4 black-text">
+                            Login
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
